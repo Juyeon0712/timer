c4318584f6e6a0499f630b630e60fc48126cce07
Juyeon0712 <wndus071268@gmail.com>
2025-07-28 16:42:32 +0900

    스타일적용

--- timer.py
+++ timer.py
@@ -77,15 +77,15 @@ with col_left:
     status_col1, status_col2, status_col3=st.columns(3)
     with status_col1:
         if current_status=="running":
-            st.markdown('타이머', help="타이머가 실행중입니다!")
+            st.markdown('**타이머**', help="타이머가 실행중입니다!")
         elif current_status=="paused":
-            st.markdown('타이머', help="타이머가 일시 정지 되었습니다!")
+            st.markdown('**타이머**', help="타이머가 일시 정지 되었습니다!")
         elif current_status=="copleted":
-            st.markdown('타이머', help="타이머가 완료되었습니다!")
+            st.markdown('**타이머**', help="타이머가 완료되었습니다!")
         else:
-            st.markdown('타이머', help="타이머가 대기중입니다!")
+            st.markdown('**타이머**', help="타이머가 대기중입니다!")
     with status_col3:
-        st.markdown(f"{int(progress*100)}%")
+        st.markdown(f"<p style="text-aligin:right;"><strong>{int(progress*100)}%<strong><p>"unsafd_allow_html=True)
 
 with col_right:
     pass
